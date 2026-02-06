8f2519b6f7458818e52f4e7e22f3d4fd260aecfd
alex.abriel <alex.abriel@epita.fr>
2026-02-06 11:57:38 +0100

    dfsfds

--- tp1/exo_6.m
+++ tp1/exo_6.m
@@ -6,15 +6,36 @@ tA = (145*pi)/180;
 tB = 0;
 tC = (-95*pi)/180;
 
+k = -5:0.1:5;
 
-k = linspace(-5, 5,100);
-
-DA = [A(1)+k*cos(tA); A(2)+k*sin(tA)];
-DB = [B(1)+k*cos(tB); B(2)+k*sin(tB)];
-DC = [C(1)+k*cos(tC); C(2)+k*sin(tC)];
+draw_line = @(P, th) plot(P(1) + k * cos(th),P(2) + k * sin (th));
 
 hold on;
-plot(DA(1,:),DA(2,:));
-plot(DB(1,:),DB(2,:));
-plot(DC(1,:),DC(2,:));
+draw_line(A, tA);
+draw_line(B, tB);
+draw_line(C, tC);
 hold off;
+
+C1 = [-sin(tA), cos(tA);
+      -sin(tB), cos(tB);
+      -sin(tC), cos(tC)];
+
+C2 = [-sin(tA)*A(1)+ cos(tA)*A(2);
+      -sin(tB)*B(1)+ cos(tB)*B(2);
+      -sin(tC)*C(1)+ cos(tC)*C(2)];
+
+Pos_navire = C1\C2;
+
+hold on;
+plot(Pos_navire(1),Pos_navire(2));
+plot(A(1),A(2));
+text(A(1) + 0.2, A(2), "Phare 1");
+plot(B(1),B(2));
+text(B(1) + 0.2, B(2), "Phare 2");
+plot(C(1),C(2));
+text(C(1) + 0.2, C(2), "Phare 3");
+text(Pos_navire(1) + 0.2, Pos_navire(2), "Navire");
+axis equal;
+grid on;
+hold off;
+
